edb51211555510060089f47fc55aca3c5b708882
LopeKinz <48872005+LopeKinz@users.noreply.github.com>
2023-04-03 09:55:43 +0200

    Update gsi.py

---
+++ src/gsi.py
@@ -0,0 +1,21 @@
+import platform,socket,re,uuid,json,psutil,logging
+
+def getSystemInfo():
+    try:
+        info={}
+        info['platform']=platform.system()
+        info['platform-release']=platform.release()
+        info['platform-version']=platform.version()
+        info['architecture']=platform.machine()
+        info['hostname']=socket.gethostname()
+        info['ip-address']=socket.gethostbyname(socket.gethostname())
+        info['mac-address']=':'.join(re.findall('..', '%012x' % uuid.getnode()))
+        info['processor']=platform.processor()
+        info['ram']=str(round(psutil.virtual_memory().total / (1024.0 **3)))+" GB"
+        return json.dumps(info)
+    except Exception as e:
+        logging.exception(e)
+
+json.loads(getSystemInfo())
+
+#add webhook funktion
