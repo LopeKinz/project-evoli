aff8a486f35439bd3fab2131ebed6c6a224af8fe
LopeKinz <48872005+LopeKinz@users.noreply.github.com>
2023-04-03 10:03:32 +0200

    Create check_port.py

---
+++ check_port.py
@@ -0,0 +1,9 @@
+import socket
+def isOpen(ip,port):
+   s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+   try:
+      s.connect((ip, int(port)))
+      s.shutdown(2)
+      return True
+   except:
+      return False
