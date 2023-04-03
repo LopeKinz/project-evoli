f37be9250ebf3c8f5496dc0d6a994179eeb4121c
LopeKinz <48872005+LopeKinz@users.noreply.github.com>
2023-04-03 09:50:21 +0200

    added ask for admin

---
+++ src/afa.py
@@ -0,0 +1,13 @@
+import ctypes, sys
+
+def is_admin():
+    try:
+        return ctypes.windll.shell32.IsUserAnAdmin()
+    except:
+        return False
+
+if is_admin():
+    # dicord webhook response !!
+else:
+    # Re-run the program with admin rights
+    ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)
