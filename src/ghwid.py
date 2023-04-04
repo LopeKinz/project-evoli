c0b1d48dd16deb5d0949ae8dc17a1bd636dd1f81
LopeKinz <48872005+LopeKinz@users.noreply.github.com>
2023-04-04 09:59:52 +0200

    Update ghwid.py

--- src/ghwid.py
+++ src/ghwid.py
@@ -1,5 +1,5 @@
 import os, subprocess
-def getLisencekey():
+def get_hwid():
     if os.name == 'nt':
         hwid = subprocess.check_output('wmic csproduct get uuid').decode().split('\n')[1].strip()
         return hwid
