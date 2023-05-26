1fd702d11cf221b9d042eca35cdc93c4f72ebfb5
Scooby <100179759+scoobyluvs@users.noreply.github.com>
2023-05-25 20:06:29 -0500

    Update afa.py

--- src/afa.py
+++ src/afa.py
@@ -2,18 +2,27 @@ import ctypes
 import sys
 
 
-class AFA:
-    
+class AdminRightsChecker:
+
     @staticmethod
-    def check():
+    def check() -> str:
+        
+        """
+        Asks for admin.
+
+        Returns:
+            str: "Already Admin" if the user has administrator rights, or "Asked For Admin" if it asked for admin duh.
+        """
+        
         if ctypes.windll.shell32.IsUserAnAdmin():
             return "Already Admin"
         else:
             ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)
             return "Asked For Admin"
 
+# test case 
 
 if __name__ == "__main__":
-    main = AFA()
+    main = AdminRightsChecker()
     result = main.check()
     print(result)
