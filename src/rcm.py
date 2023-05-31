497756b98895d63f09c692fbcf964d098ad0a7d6
LopeKinz <48872005+LopeKinz@users.noreply.github.com>
2023-05-31 15:36:26 +0200

    Update rcm.py

---
+++ src/rcm.py
@@ -0,0 +1,8 @@
+import win32api
+import win32con
+
+def create_remote_message_box(computer_name, message, title):
+    win32api.MessageBox(win32con.NULL, message, title, win32con.MB_OK | win32con.MB_ICONINFORMATION, 0, computer_name)
+
+
+# Not DONE!
