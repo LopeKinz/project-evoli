47a2c6c674d50e867cc1db5bfd4c7d2d25c367c1
LopeKinz <48872005+LopeKinz@users.noreply.github.com>
2023-06-13 15:48:18 +0200

    Create inputs.py

---
+++ src/inputs.py
@@ -0,0 +1,15 @@
+import ctypes
+            is_admin = ctypes.windll.shell32.IsUserAnAdmin() != 0
+            if is_admin == True:
+                ok = windll.user32.BlockInput(True)
+                return "Blocked"
+            else:
+                return "No Admin"
+def unblock():
+            import ctypes
+            is_admin = ctypes.windll.shell32.IsUserAnAdmin() != 0
+            if is_admin == True:
+                ok = windll.user32.BlockInput(False)
+                return "Unblocked"
+            else:
+                return "No Admin"
