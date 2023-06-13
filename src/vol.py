1cc2ddc93dff5ac00867889527a13f8eb5a7eece
LopeKinz <48872005+LopeKinz@users.noreply.github.com>
2023-06-13 15:45:32 +0200

    Create vol.py

---
+++ src/vol.py
@@ -0,0 +1,15 @@
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+
+def volumeup():
+    devices = AudioUtilities.GetSpeakers()
+    interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+    volume = cast(interface, POINTER(IAudioEndpointVolume))
+    if volume.GetMute() == 1:
+        volume.SetMute(0, None)
+    volume.SetMasterVolumeLevel(volume.GetVolumeRange()[1], None)
+
+def volumedown():
+    devices = AudioUtilities.GetSpeakers()
+    interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+    volume = cast(interface, POINTER(IAudioEndpointVolume))
+    volume.SetMasterVolumeLevel(volume.GetVolumeRange()[0], None)
