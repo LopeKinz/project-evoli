8730d82fe192960c244c836080e391afe3c18edc
LopeKinz <48872005+LopeKinz@users.noreply.github.com>
2023-06-13 15:46:30 +0200

    Create clip.py

---
+++ src/clip.py
@@ -0,0 +1,20 @@
+import ctypes
+import os
+def get_clip():
+            CF_TEXT = 1
+            kernel32 = ctypes.windll.kernel32
+            kernel32.GlobalLock.argtypes = [ctypes.c_void_p]
+            kernel32.GlobalLock.restype = ctypes.c_void_p
+            kernel32.GlobalUnlock.argtypes = [ctypes.c_void_p]
+            user32 = ctypes.windll.user32
+            user32.GetClipboardData.restype = ctypes.c_void_p
+            user32.OpenClipboard(0)
+            if user32.IsClipboardFormatAvailable(CF_TEXT):
+                data = user32.GetClipboardData(CF_TEXT)
+                data_locked = kernel32.GlobalLock(data)
+                text = ctypes.c_char_p(data_locked)
+                value = text.value
+                kernel32.GlobalUnlock(data_locked)
+                body = value.decode()
+                user32.CloseClipboard()
+                return body
